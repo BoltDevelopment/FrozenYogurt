66b0b4229dcc194b1282f495eecd276d6f76d417
Scriptedev <>
2021-04-30 16:39:03 +0000

    Added Help Command

---
+++ commands/help.js
@@ -0,0 +1,37 @@
+const { MessageEmbed } = require('discord.js')
+const db = require('quick.db')
+module.exports = {
+	name: 'help',
+	description: 'Sends info about the bot.',
+	execute(c, m, a) {
+    var prefix = db.get(`prefix_${m.guild.id}`)
+    if(prefix == null) prefix = '&'
+		const pagination =  require('discord.js-pagination');
+
+const page1 = new MessageEmbed()
+page1.setAuthor("📬 Need help?", c.user.displayAvatarURL())
+page1.setThumbnail(c.user.displayAvatarURL())
+ page1.setDescription(`Use \`${prefix}help\` followed by a command name to get more additional information on a command. For example: \`${prefix}help [Command].\`\n\n[Invite me](TEST) **|** [Support Server](TEST) **|** [Documention](TEST) **|** [Website](TEST)`)
+page1.setFooter(m.author.tag, m.author.displayAvatarURL({ dynamic: true }))
+page1.setTimestamp()
+//page1.setColor(roleColor);
+
+const embed2 = new MessageEmbed()
+.setAuthor(c.user.tag, c.user.displayAvatarURL())
+.setThumbnail(c.user.displayAvatarURL())
+.setTitle('All Commands.')
+.addFields('NOT DONE')
+//.setColor(roleColor)
+
+const pages = [
+    page1,
+    embed2
+];
+
+const emojiList = ['⏮', '⏭']
+const timeout = 120000;
+
+pagination(m, pages, emojiList, timeout);
+
+	},
+};
